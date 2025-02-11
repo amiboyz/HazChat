3ae3d6f6f654ae7b30036ea7c999ce7cc6c7e23a
amiboyz <152479402+amiboyz@users.noreply.github.com>
2025-02-11 07:18:45 +0700

    Update HazChat_ver1.py

--- HazChat/HazChat_ver1.py
+++ HazChat/HazChat_ver1.py
@@ -224,7 +224,7 @@ def get_response(provider, client, prompt, role, vector_store, prompt_laws, prom
 
         if provider == "OpenAI":
             response = client.chat.completions.create(
-                model="gpt-4o-mini",
+                model="GPT-4o",
                 messages=[{"role": "user", "content": augmented_prompt}]
             )
             token_usage = response.usage.total_tokens  # OpenAI API memberikan jumlah token
